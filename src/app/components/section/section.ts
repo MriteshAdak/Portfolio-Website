175fe9397ca9ce3097f47a41d7854ee81f7994a0
MriteshAdak <mriteshadak@outlook.com>
2026-03-30 23:40:44 +0200

    Add Section component and its corresponding unit tests

---
+++ src/app/components/section/section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  imports: [],
+  template: ` <p>section works!</p> `,
+  styles: ``,
+})
+export class Section {}
